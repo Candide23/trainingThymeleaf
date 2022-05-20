61ef20f7e15b8ac9d2a4c6476add1c55e7aecc96
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-05-20 11:24:09 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/TrainingThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingThymeleafApplication.class, args);
+	}
+
+}
